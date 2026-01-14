d54a636e2094c7ec2317337c3e778e1e35d71938
Danylo DANYLIUK <danylo.danyliuk@ensta-paris.fr>
2026-01-14 22:15:24 +0200

    feat: add multiplayer + AI support

--- Game.cpp
+++ Game.cpp
@@ -5,15 +5,26 @@
 
 Game::Game()
 {
-    grid = Grid();
-    blocks = GetAllBlocks();
-    currentBlock = GetRandomBlock();
-    nextBlock = GetRandomBlock();
+    for (int i = 0; i < 2; i++) {
+        grid[i] = Grid();
+        blocks[i] = GetAllBlocks();
+        currentBlock[i] = GetRandomBlock(i);
+        nextBlock[i] = GetRandomBlock(i);
+        score[i] = 0;
+        linesClearedTotal[i] = 0;
+        level[i] = 1;
+        canHold[i] = true;
+        hasHoldBlock[i] = false;
+    }
+
     gameOver = false;
     paused = false;
-    score = 0;
-    linesClearedTotal = 0;
-    level = 1;
+    isMultiplayer = false;
+    isAIActive = false;
+    lastAIMoveTime = 0;
+    targetColumn = -1;
+    targetRotation = -1;
+
     LoadHighScore();
 
     InitAudioDevice();
@@ -33,34 +44,65 @@ Game::~Game()
 
 void Game::Draw()
 {
-    grid.Draw();
-    if (!gameOver && !paused) {
-        DrawGhostBlock();
+    int offsetsX[2];
+    if (isMultiplayer || isAIActive) {
+        offsetsX[0] = 11;
+        offsetsX[1] = 511;
+    } else {
+        offsetsX[0] = 11;
+        offsetsX[1] = 0;
     }
-    currentBlock.Draw(11, 11);
 
-    // Draw next block in the preview area
-    // The box is at {320, 225, 170, 140} in main.cpp
-    // We center the block inside this box
-    switch (nextBlock.id)
-    {
-    case 1: // IBlock
-        nextBlock.DrawPreview(335, 255);
-        break;
-    case 4: // OBlock
-        nextBlock.DrawPreview(350, 265);
-        break;
-    default:
-        nextBlock.DrawPreview(350, 255);
-        break;
+    int count = (isMultiplayer || isAIActive) ? 2 : 1;
+
+    for (int i = 0; i < count; i++) {
+        grid[i].Draw(offsetsX[i], 11);
+        if (!gameOver && !paused) {
+            DrawGhostBlock(i);
+        }
+        currentBlock[i].Draw(offsetsX[i], 11);
+
+        // Draw next block preview
+        int nextX = offsetsX[i] + 320;
+        int nextY = 225;
+
+        int drawX = nextX + 50;
+        int drawY = nextY + 45;
+        if (nextBlock[i].id == 1) { // IBlock
+            drawX = nextX + 35;
+            drawY = nextY + 45;
+        } else if (nextBlock[i].id == 4) { // OBlock
+            drawX = nextX + 50;
+            drawY = nextY + 55;
+        }
+        nextBlock[i].DrawPreview(drawX, drawY);
+
+        // Draw hold block preview
+        if (hasHoldBlock[i]) {
+            int holdX = offsetsX[i] + 320;
+            int holdY = 495;
+
+            // Centering logic for HOLD block
+            int hDrawX = holdX + 50;
+            int hDrawY = holdY + 45;
+            if (holdBlock[i].id == 1) { // IBlock
+                hDrawX = holdX + 35;
+                hDrawY = holdY + 45;
+            } else if (holdBlock[i].id == 4) { // OBlock
+                hDrawX = holdX + 50;
+                hDrawY = holdY + 55;
+            }
+            holdBlock[i].DrawPreview(hDrawX, hDrawY);
+        }
     }
 }
 
-void Game::DrawGhostBlock()
+void Game::DrawGhostBlock(int playerIdx)
 {
-    Block ghostBlock = currentBlock;
+    int offsetX = (isMultiplayer || isAIActive) ? ((playerIdx == 0) ? 11 : 511) : 11;
+    Block ghostBlock = currentBlock[playerIdx];
     int offset = 0;
-    while (BlockMovementValid(offset + 1, 0))
+    while (BlockMovementValid(playerIdx, offset + 1, 0))
     {
         offset++;
     }
@@ -68,19 +110,19 @@ void Game::DrawGhostBlock()
     std::vector<Position> tiles = ghostBlock.GetCellPositions();
     for (Position item : tiles)
     {
-        DrawRectangle((item.column) * 30 + 11, (item.row + offset) * 30 + 11, 29, 29, Fade(ghostBlock.colors[ghostBlock.id], 0.3f));
+        DrawRectangle((item.column) * 30 + offsetX, (item.row + offset) * 30 + 11, 29, 29, Fade(ghostBlock.colors[ghostBlock.id], 0.3f));
     }
 }
 
-Block Game::GetRandomBlock()
+Block Game::GetRandomBlock(int playerIdx)
 {
-    if (blocks.empty())
+    if (blocks[playerIdx].empty())
     {
-        blocks = GetAllBlocks();
+        blocks[playerIdx] = GetAllBlocks();
     }
-    int randomIndex = rand() % blocks.size();
-    Block block = blocks[randomIndex];
-    blocks.erase(blocks.begin() + randomIndex);
+    int randomIndex = rand() % blocks[playerIdx].size();
+    Block block = blocks[playerIdx][randomIndex];
+    blocks[playerIdx].erase(blocks[playerIdx].begin() + randomIndex);
     return block;
 }
 
@@ -89,12 +131,12 @@ std::vector<Block> Game::GetAllBlocks()
     return {IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()};
 }
 
-bool Game::BlockFits()
+bool Game::BlockFits(int playerIdx)
 {
-    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    std::vector<Position> tiles = currentBlock[playerIdx].GetCellPositions();
     for (Position item : tiles)
     {
-        if (grid.IsCellOutside(item.row, item.column) || !grid.IsCellEmpty(item.row, item.column))
+        if (grid[playerIdx].IsCellOutside(item.row, item.column) || !grid[playerIdx].IsCellEmpty(item.row, item.column))
         {
             return false;
         }
@@ -102,42 +144,42 @@ bool Game::BlockFits()
     return true;
 }
 
-bool Game::BlockMovementValid(int rows, int columns)
+bool Game::BlockMovementValid(int playerIdx, int rows, int columns)
 {
-    currentBlock.Move(rows, columns);
-    bool fits = BlockFits();
-    currentBlock.Move(-rows, -columns);
+    currentBlock[playerIdx].Move(rows, columns);
+    bool fits = BlockFits(playerIdx);
+    currentBlock[playerIdx].Move(-rows, -columns);
     return fits;
 }
 
-void Game::MoveBlockLeft()
+void Game::MoveBlockLeft(int playerIdx)
 {
-    if (!gameOver && !paused && BlockMovementValid(0, -1))
+    if (!gameOver && !paused && BlockMovementValid(playerIdx, 0, -1))
     {
-        currentBlock.Move(0, -1);
+        currentBlock[playerIdx].Move(0, -1);
     }
 }
 
-void Game::MoveBlockRight()
+void Game::MoveBlockRight(int playerIdx)
 {
-    if (!gameOver && !paused && BlockMovementValid(0, 1))
+    if (!gameOver && !paused && BlockMovementValid(playerIdx, 0, 1))
     {
-        currentBlock.Move(0, 1);
+        currentBlock[playerIdx].Move(0, 1);
     }
 }
 
-void Game::MoveBlockDown()
+void Game::MoveBlockDown(int playerIdx)
 {
     if (gameOver || paused) return;
 
-    if (BlockMovementValid(1, 0))
+    if (BlockMovementValid(playerIdx, 1, 0))
     {
-        currentBlock.Move(1, 0);
-        UpdateScore(0, 1);
+        currentBlock[playerIdx].Move(1, 0);
+        UpdateScore(playerIdx, 0, 1);
     }
     else
     {
-        LockBlock();
+        LockBlock(playerIdx);
     }
 }
 
@@ -154,44 +196,48 @@ void Game::HandleInput()
         Reset();
     }
 
-    if (!paused && keyPressed != 0)
+    if (!paused)
     {
-        switch (keyPressed)
-        {
-        case KEY_LEFT:
-            MoveBlockLeft();
-            break;
-        case KEY_RIGHT:
-            MoveBlockRight();
-            break;
-        case KEY_DOWN:
-            MoveBlockDown();
-            break;
-        case KEY_UP:
-            RotateBlock();
-            break;
-        case KEY_SPACE:
-            // Hard drop
-            while (BlockMovementValid(1, 0)) {
-                currentBlock.Move(1, 0);
-                UpdateScore(0, 2);
+        // Player 1 Controls
+        if (IsKeyPressed(KEY_LEFT)) MoveBlockLeft(0);
+        if (IsKeyPressed(KEY_RIGHT)) MoveBlockRight(0);
+        if (IsKeyPressed(KEY_DOWN)) MoveBlockDown(0);
+        if (IsKeyPressed(KEY_UP)) RotateBlock(0);
+        if (IsKeyPressed(KEY_C)) HoldBlock(0);
+        if (IsKeyPressed(KEY_SPACE)) {
+            while (BlockMovementValid(0, 1, 0)) {
+                currentBlock[0].Move(1, 0);
+                UpdateScore(0, 0, 2);
             }
-            LockBlock();
-            break;
-        default:
-            break;
+            LockBlock(0);
+        }
+
+        // Player 2 Controls (only if multiplayer)
+        if (isMultiplayer) {
+            if (IsKeyPressed(KEY_A)) MoveBlockLeft(1);
+            if (IsKeyPressed(KEY_D)) MoveBlockRight(1);
+            if (IsKeyPressed(KEY_S)) MoveBlockDown(1);
+            if (IsKeyPressed(KEY_W)) RotateBlock(1);
+            if (IsKeyPressed(KEY_R)) HoldBlock(1);
+            if (IsKeyPressed(KEY_F)) {
+                while (BlockMovementValid(1, 1, 0)) {
+                    currentBlock[1].Move(1, 0);
+                    UpdateScore(1, 0, 2);
+                }
+                LockBlock(1);
+            }
         }
     }
 }
 
-void Game::RotateBlock()
+void Game::RotateBlock(int playerIdx)
 {
     if (gameOver || paused) return;
 
-    currentBlock.Rotate();
-    if (BlockFits() == false)
+    currentBlock[playerIdx].Rotate();
+    if (BlockFits(playerIdx) == false)
     {
-        currentBlock.UndoRotation();
+        currentBlock[playerIdx].UndoRotation();
     }
     else
     {
@@ -199,68 +245,105 @@ void Game::RotateBlock()
     }
 }
 
-void Game::LockBlock()
+void Game::LockBlock(int playerIdx)
 {
-    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    std::vector<Position> tiles = currentBlock[playerIdx].GetCellPositions();
     for (Position item : tiles)
     {
-        grid.SetCellValue(item.row, item.column, currentBlock.id);
+        grid[playerIdx].SetCellValue(item.row, item.column, currentBlock[playerIdx].id);
     }
-    int rowsCleared = grid.ClearFullRows();
+    int rowsCleared = grid[playerIdx].ClearFullRows();
     if (rowsCleared > 0)
     {
         PlaySound(clearSound);
     }
-    UpdateScore(rowsCleared, 0);
+    UpdateScore(playerIdx, rowsCleared, 0);
 
-    currentBlock = nextBlock;
-    if (BlockFits() == false)
+    currentBlock[playerIdx] = nextBlock[playerIdx];
+    canHold[playerIdx] = true;
+    if (BlockFits(playerIdx) == false)
     {
         gameOver = true;
-        if (score > highscore) {
-            highscore = score;
+        if (score[0] > highscore) {
+            highscore = score[0];
+            SaveHighScore();
+        }
+        if (isMultiplayer && score[1] > highscore) {
+            highscore = score[1];
             SaveHighScore();
         }
     }
-    nextBlock = GetRandomBlock();
+    nextBlock[playerIdx] = GetRandomBlock(playerIdx);
+
+    if (playerIdx == 1 && isAIActive) {
+        targetColumn = -1;
+        targetRotation = -1;
+    }
+}
+
+void Game::HoldBlock(int playerIdx)
+{
+    if (!canHold[playerIdx] || gameOver || paused) return;
+
+    Block temp;
+    bool hadHold = hasHoldBlock[playerIdx];
+
+    if (!hadHold) {
+        holdBlock[playerIdx] = currentBlock[playerIdx];
+        holdBlock[playerIdx].Move(-holdBlock[playerIdx].GetCellPositions()[0].row + 0, -holdBlock[playerIdx].GetCellPositions()[0].column + 0);
+
+        currentBlock[playerIdx] = nextBlock[playerIdx];
+        nextBlock[playerIdx] = GetRandomBlock(playerIdx);
+        hasHoldBlock[playerIdx] = true;
+    } else {
+        temp = currentBlock[playerIdx];
+        currentBlock[playerIdx] = holdBlock[playerIdx];
+        currentBlock[playerIdx].Move(-currentBlock[playerIdx].GetCellPositions()[0].row + 0, -currentBlock[playerIdx].GetCellPositions()[0].column + 3);
+        holdBlock[playerIdx] = temp;
+    }
+    canHold[playerIdx] = false;
+
+    if (playerIdx == 1 && isAIActive) {
+        targetColumn = -1;
+        targetRotation = -1;
+    }
 }
 
 void Game::Reset()
 {
-    grid.Initialize();
-    blocks = GetAllBlocks();
-    currentBlock = GetRandomBlock();
-    nextBlock = GetRandomBlock();
-    score = 0;
-    linesClearedTotal = 0;
-    level = 1;
+    for (int i = 0; i < 2; i++) {
+        grid[i].Initialize();
+        blocks[i] = GetAllBlocks();
+        currentBlock[i] = GetRandomBlock(i);
+        nextBlock[i] = GetRandomBlock(i);
+        score[i] = 0;
+        linesClearedTotal[i] = 0;
+        level[i] = 1;
+        canHold[i] = true;
+        hasHoldBlock[i] = false;
+    }
     gameOver = false;
     paused = false;
+    targetColumn = -1;
+    targetRotation = -1;
 }
 
-void Game::UpdateScore(int linesCleared, int moveDownPoints)
+void Game::UpdateScore(int playerIdx, int linesCleared, int moveDownPoints)
 {
+    int basePoints = 0;
     switch (linesCleared)
     {
-    case 1:
-        score += 100 * level;
-        break;
-    case 2:
-        score += 300 * level;
-        break;
-    case 3:
-        score += 500 * level;
-        break;
-    case 4:
-        score += 800 * level;
-        break;
-    default:
-        break;
+    case 1: basePoints = 100; break;
+    case 2: basePoints = 300; break;
+    case 3: basePoints = 500; break;
+    case 4: basePoints = 800; break;
     }
-    score += moveDownPoints;
 
-    linesClearedTotal += linesCleared;
-    level = 1 + (linesClearedTotal / 10);
+    score[playerIdx] += basePoints * level[playerIdx];
+    score[playerIdx] += moveDownPoints;
+
+    linesClearedTotal[playerIdx] += linesCleared;
+    level[playerIdx] = 1 + (linesClearedTotal[playerIdx] / 10);
 }
 
 void Game::SaveHighScore()
@@ -280,5 +363,138 @@ void Game::LoadHighScore()
         file.close();
     } else {
         highscore = 0;
+    }
+}
+
+// --- AI Implementation ---
+
+float Game::EvaluateBoard(const Grid& tempGrid) {
+    int aggregateHeight = 0;
+    int holes = 0;
+    int bumpiness = 0;
+    int completeLines = 0;
+
+    int columnHeights[10] = {0};
+
+    for (int c = 0; c < 10; c++) {
+        for (int r = 0; r < 20; r++) {
+            if (tempGrid.grid[r][c] != 0) {
+                columnHeights[c] = 20 - r;
+                break;
+            }
+        }
+        aggregateHeight += columnHeights[c];
+    }
+
+    for (int c = 0; c < 10; c++) {
+        bool blockFound = false;
+        for (int r = 0; r < 20; r++) {
+            if (tempGrid.grid[r][c] != 0) blockFound = true;
+            else if (blockFound && tempGrid.grid[r][c] == 0) holes++;
+        }
+    }
+
+    for (int c = 0; c < 9; c++) {
+        bumpiness += abs(columnHeights[c] - columnHeights[c+1]);
+    }
+
+    for (int r = 0; r < 20; r++) {
+        bool full = true;
+        for (int c = 0; c < 10; c++) {
+            if (tempGrid.grid[r][c] == 0) {
+                full = false;
+                break;
+            }
+        }
+        if (full) completeLines++;
+    }
+
+    return -0.51f * aggregateHeight + 0.76f * completeLines - 0.35f * holes - 0.18f * bumpiness;
+}
+
+Game::AIMove Game::GetBestMove() {
+    AIMove bestMove = {0, 0, -1000000.0f};
+    Block originalBlock = currentBlock[1];
+
+    for (int r = 0; r < 4; r++) {
+        Block testBlock = originalBlock;
+        for (int i = 0; i < r; i++) testBlock.Rotate();
+
+        for (int c = -3; c < 10; c++) {
+            Block moveBlock = testBlock;
+            moveBlock.Move(0, c - 3);
+
+            bool valid = true;
+            for (auto p : moveBlock.GetCellPositions()) {
+                if (grid[1].IsCellOutside(p.row, p.column) || !grid[1].IsCellEmpty(p.row, p.column)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid) continue;
+
+            int drop = 0;
+            while (true) {
+                bool canDrop = true;
+                for (auto p : moveBlock.GetCellPositions()) {
+                    if (grid[1].IsCellOutside(p.row + drop + 1, p.column) || !grid[1].IsCellEmpty(p.row + drop + 1, p.column)) {
+                        canDrop = false;
+                        break;
+                    }
+                }
+                if (canDrop) drop++;
+                else break;
+            }
+
+            Grid tempGrid = grid[1];
+            for (auto p : moveBlock.GetCellPositions()) {
+                tempGrid.SetCellValue(p.row + drop, p.column, moveBlock.id);
+            }
+
+            float score = EvaluateBoard(tempGrid);
+            if (score > bestMove.score) {
+                bestMove.score = score;
+                bestMove.rotation = r;
+                bestMove.column = c;
+            }
+        }
+    }
+    return bestMove;
+}
+
+void Game::UpdateAI() {
+    if (!isAIActive || gameOver || paused) return;
+
+    double currentTime = GetTime();
+    if (currentTime - lastAIMoveTime < 0.25) return;
+    lastAIMoveTime = currentTime;
+
+    if (targetColumn == -1) {
+        AIMove move = GetBestMove();
+        targetRotation = move.rotation;
+        targetColumn = move.column;
+    }
+
+    if (targetRotation > 0) {
+        RotateBlock(1);
+        targetRotation--;
+        return;
+    }
+
+    int currentMinCol = 10;
+    for (auto p : currentBlock[1].GetCellPositions()) {
+        if (p.column < currentMinCol) currentMinCol = p.column;
+    }
+
+    if (currentMinCol < targetColumn) {
+        MoveBlockRight(1);
+    } else if (currentMinCol > targetColumn) {
+        MoveBlockLeft(1);
+    } else {
+        if (BlockMovementValid(1, 1, 0)) {
+            currentBlock[1].Move(1, 0);
+        } else {
+            LockBlock(1);
+        }
     }
 }
--- Game.h
+++ Game.h
@@ -13,37 +13,59 @@ public:
     ~Game();
     void Draw();
     void HandleInput();
-    void MoveBlockLeft();
-    void MoveBlockRight();
-    void MoveBlockDown();
-    void LockBlock();
+    void MoveBlockLeft(int playerIdx);
+    void MoveBlockRight(int playerIdx);
+    void MoveBlockDown(int playerIdx);
+    void LockBlock(int playerIdx);
     void Reset();
-    void RotateBlock();
-    bool BlockMovementValid(int rows, int columns);
-    bool BlockFits();
+    void RotateBlock(int playerIdx);
+    void HoldBlock(int playerIdx);
+    bool BlockMovementValid(int playerIdx, int rows, int columns);
+    bool BlockFits(int playerIdx);
+
+    // AI Methods
+    void UpdateAI();
+    float EvaluateBoard(const Grid& tempGrid);
+    struct AIMove {
+        int rotation;
+        int column;
+        float score;
+    };
+    AIMove GetBestMove();
 
     bool gameOver;
     bool paused;
-    int score;
+    bool isMultiplayer;
+    bool isAIActive;
+
+    int score[2];
     int highscore;
-    int linesClearedTotal;
-    int level;
+    int linesClearedTotal[2];
+    int level[2];
     Music music;
 
 private:
-    Grid grid;
+    Grid grid[2];
     Sound rotateSound;
     Sound clearSound;
-    std::vector<Block> blocks;
-    Block currentBlock;
-    Block nextBlock;
+    std::vector<Block> blocks[2];
+    Block currentBlock[2];
+    Block nextBlock[2];
+    Block holdBlock[2];
+    bool canHold[2];
+    bool hasHoldBlock[2];
 
-    Block GetRandomBlock();
+    Block GetRandomBlock(int playerIdx);
     std::vector<Block> GetAllBlocks();
-    void UpdateScore(int linesCleared, int moveDownPoints);
-    void DrawGhostBlock();
+    void UpdateScore(int playerIdx, int linesCleared, int moveDownPoints);
+    void DrawGhostBlock(int playerIdx);
     void SaveHighScore();
     void LoadHighScore();
+
+    // AI specific
+    double lastAIMoveTime;
+    int targetColumn;
+    int targetRotation;
 };
 
 #endif // GAME_H_INCLUDED
